eb63715b9c0f1b4e5ed9d13c288b6e4a7406eb77
ElGarkarGit <idv22c.sgranados@uartesdigitales.edu.mx>
2023-01-26 19:34:02 -0600

    Capitulo 6
    Se terminaron los ejercicios del capítulo 6.

--- Capitulo6/Source.cpp
+++ Capitulo6/Source.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <algorithm>
 #include <cstdlib>
+#include <random>
 
 
 using std::cout;
@@ -12,6 +13,7 @@ using std::endl;
 using std::string;
 using std::vector;
 using std::runtime_error;
+using std::default_random_engine;
 using std::cerr;
 
 void EjercicioUno();
@@ -29,10 +31,10 @@ double expression();
 double term();
 double primary();
 
-string sentence();
-string conjunction();
-string noun();
-string verb();
+bool sentence();
+bool conjunction();
+bool noun();
+bool verb();
 
 void error(string);
 
@@ -64,6 +66,22 @@ private:
 Token_stream::Token_stream():
 	full(false), buffer(0) {}
 
+//Clase ocupada por el ejercicio seis, itera por un string.
+class String_stream {
+public:
+	String_stream();
+	string get();
+	void putback(string s);
+private:
+	bool full;
+	string buffer;
+};
+
+//Constructor de la clase String_stream.
+String_stream::String_stream():
+	full(false), buffer("") {}
+
+
 //Clase ocupada por el ejercicio cuatro, combina lo que serian dos vectores en uno solo para la modificacion del ejercicio 19 del capitulo 4.
 class Name_value {
 public:
@@ -74,10 +92,11 @@ public:
 };
 
 Token_stream ts;
+String_stream ss;
 
 int main() {
 	int opcion;
-	cout << "Introduzca el numero del ejercicio al que desea acceder (del 1 al 14)." << endl;
+	cout << "Introduzca el numero del ejercicio al que desea acceder (del 1 al 10)." << endl;
 	cin >> opcion;
 	switch (opcion) {
 	case 1:
@@ -141,18 +160,6 @@ void EjercicioUno() {
 		while (cin) {
 			Token t = ts.get();
 
-
-			/*if (t.kind == 'q') {
-				break;
-			}
-			if (t.kind == '=') {
-				cout << "=" << val << endl;
-				ts.putback(Token('8', val));
-			}
-			else {
-				ts.putback(t);
-			}*/
-
 			while (t.kind == '=') t = ts.get();
 			if (t.kind == 'q') {
 				break;
@@ -214,17 +221,18 @@ void EjercicioCuatro() {
 	}
 }
 
+//Ejercicio que le agrega el pronombre "The" a la gramática del lenguaje inglés vista en el libro.
 void EjercicioCinco() {
 	/*Gramatica del lenguaje ingles:
 	Sentence:
-		Subject Verb
-		"the" Subject Verb
-		Sentence Conjunction Sentence
+		Subject Verb .
+		"The" Subject Verb .
+		Sentence Conjunction Sentence .
 	
 	Conjunction:
 		"and"
 		"or"
-		but"
+		"but"
 	
 	Noun:
 		"birds"
@@ -237,25 +245,226 @@ void EjercicioCinco() {
 		"swim"*/
 }
 
+//Este ejercicio adapta la gramatica de inglés utilizada en el libro para la creación de un programa que verifica si un texto cumple con la gramatica.
 void EjercicioSeis() {
-
+	cout << "Introduzca una frase. Terminela con un espacio seguido de un punto." << endl;
+	bool terminar = false;
+	while (!terminar) {
+		bool estado = sentence();
+		if (!estado) {
+			cout << "Not OK" << endl;
+			terminar = true;
+		}
+		string s = ss.get();
+		if (s == ".") {
+			cout << "OK" << endl;
+			terminar = true;
+		}
+		else {
+			ss.putback(s);
+			estado = conjunction();
+			if (!estado) {
+				cout << "Not OK" << endl;
+				terminar = true;
+			}
+		}
+	}
 }
 
+//Ejercicio que establece una gramática para las operaciones lógicas de bits.
 void EjercicioSiete() {
 	/*Gramatica de operaciones logicas de bits:
+	OR:
+		XOR
+		OR '|' XOR
+
+	XOR:
+		AND
+		XOR '^' AND
+
+	AND:
+		Unary
+		AND '&' Unary
+	
+	Unary:
+		Primary
+		Unary '!' Primary
+		Unary '~' Primary
+	
+	Primary:
+		Number
+
+	Number:
+		floating-point-literal
 	*/
 }
 
+//Ejercicio que modifica el ejercicio 12 del capítulo 5 para que acepte letras en vez de números.
 void EjercicioOcho() {
+	vector<char> adivinanza;
+	vector<char> intento;
+	vector <char> numeros = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
+	char entrada = ' ';
+	int i = 0, bulls = 0, cows = 0, intentos = 0, seed = 0;
+	bool repetido = false;
 
+	cout << "Introduzca un valor para generar la secuencia aleatorio." << endl;
+	cin >> seed;
+	srand(seed);
+
+	shuffle(numeros.begin(), numeros.end(), default_random_engine(seed));
+
+	for (int i = 0; i < 4; ++i) {
+		adivinanza.push_back(numeros[i]);
+	}
+
+	//cout << "intento: " << adivinanza[0] << " " << adivinanza[1] << " " << adivinanza[2] << " " << adivinanza[3] << endl;
+
+	while (bulls < 4) {
+		intento.clear();
+		bulls = 0;
+		cows = 0;
+		i = 0;
+		repetido = false;
+
+		cout << "Introduzca cuatro letras. (No pueden estar repetidas)" << endl;
+		while (i < 4) {
+			cin >> entrada;
+			if (cin.fail()) {
+				cin.clear();
+				cin.ignore(INT_MAX, '\n');
+				cout << "El valor introducido no es una letra, intentelo nuevamente." << endl;
+			}
+			else if (int(entrada) < 60 || int(entrada) > 123) {
+				cout << "El valor introducido no es una letra, intentelo nuevamente." << endl;
+			}
+			else {
+				if (intento.empty()) {
+					intento.push_back(entrada);
+					++i;
+				}
+				else {
+					for (int x : intento) {
+						if (entrada == x) {
+							repetido = true;
+						}
+					}
+					if (repetido) {
+						cout << "La letra introducida esta repetida, introduzca otro." << endl;
+					}
+					else {
+						intento.push_back(entrada);
+						++i;
+					}
+					repetido = false;
+				}
+			}
+		}
+		cout << "intento: " << intento[0] << intento[1] << intento[2] << intento[3] << endl;
+
+
+		for (int i = 0; i < adivinanza.size(); ++i) {
+			for (int j = 0; j < intento.size(); ++j) {
+				if (adivinanza[i] == intento[j]) {
+					if (i == j) {
+						++bulls;
+					}
+					else {
+						++cows;
+					}
+				}
+			}
+		}
+
+		cout << "Hay " << bulls << " Toros y " << cows << " Vacas." << endl;
+		++intentos;
+	}
+
+	cout << "Has ganado! La secuencia secreta era " << adivinanza[0] << adivinanza[1] << adivinanza[2] << adivinanza[3] << endl;
+	cout << "Te ha tomado " << intentos << " intentos adivinar la secuencia." << endl;
 }
 
+//Ejercicio que lee digitos en una cadena y los convierte en enteros, imprimiendolos con la cantidad que representan.
 void EjercicioNueve() {
+	string entrada = "";
+	vector<int> numeros;
+	try {
+		cin >> entrada;
+		if (entrada.size() < 4) {
+			error("Este programa no acepta mas de 4 numeros.");
+		}
 
+		for (char ch : entrada) {
+			if (isdigit(ch)) {
+				numeros.push_back(ch - '0');
+			}
+			else {
+				error("Uno de los valores introducidos no es numerico.");
+			}
+		}
+
+		switch (numeros.size()) {
+		case 1:
+			cout << "Hay " << numeros[0] << " unos." << endl;
+			break;
+		case 2:
+			cout << "Hay " << numeros[0] << " dieces y " << numeros[1] << " unos." << endl;
+			break;
+		case 3:
+			cout << "Hay " << numeros[0] << " cienes, " << numeros[1] << " dieces y " << numeros[2] << " unos." << endl;
+			break;
+		case 4:
+			cout << "Hay " << numeros[0] << " miles, " << numeros[1] << " cienes, " << numeros[2] << " dieces y " << numeros[3] << " unos." << endl;
+			break;
+		}
+	}
+	catch (runtime_error& e) {
+		cout << e.what() << endl;
+	}
 }
 
+//Ejercicio que calcula permutaciones o combinaciones de dos numeros ocupando su fórmula y la función de factorial.
 void EjercicioDiez() {
+	int numero1 = 0, numero2 = 0;
+	double resultado;
+	char seleccion = ' ';
 
+	try {
+		cout << "Introduzca la cantidad de numeros posibles." << endl;
+		cin >> numero1;
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(INT_MAX, '\n');
+			error("El valor introducido no es un entero.");
+		}
+
+		cout << "Introduzca la cantidad de numeros que se combinaran." << endl;
+		cin >> numero2;
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(INT_MAX, '\n');
+			error("El valor introducido no es un entero.");
+		}
+
+		cout << "Introduzca 'p' para calcular la permutacion, introduzca 'c' para calcular la combinacion." << endl;
+		cin >> seleccion;
+		switch (seleccion) {
+		case 'p':
+			resultado = factorial(numero1) / (factorial(numero1 - numero2));
+			cout << "El resultado de P(" << numero1 << "," << numero2 << ") es " << resultado << endl;
+			break;
+		case 'c':
+			resultado = (factorial(numero1) / (factorial(numero1 - numero2))) / factorial(numero2);
+			cout << "El resultado de C(" << numero1 << "," << numero2 << ") es " << resultado << endl;
+			break;
+		default:
+			error("No se introdujo un valor valido.");
+			break;
+		}
+	}
+	catch (runtime_error& e) {
+		cerr << e.what() << '\n';
+	}
 }
 
 void error(string s) {
@@ -378,36 +587,71 @@ Token Token_stream::get()
 		}
 }
 
-//Esta funcion es usada por el ejercicio tres, calcula y devuelve la factorial de un numero si cumple con los requisitos.
+void String_stream::putback(string s) {
+	if (full) error("putback() into a full buffer");
+	buffer = s; // copy t to buffer
+	full = true; // buffer is now full
+}
+
+string String_stream::get() {
+	if (full) {
+		full = false;
+		return buffer;
+	}
+
+	string s;
+	cin >> s;
+	return s;
+}
+
+//Esta función es usada por el ejercicio tres, calcula y devuelve la factorial de un numero si cumple con los requisitos.
 double factorial(double num) {
-	int check = num;
-	if (double(check) != num) {
+	if (int(num) != num) {
 		error("No se puede calcular factorial para decimales.");
 	}
 	else if (num == 0) {
 		return 1;
 	}
 	else {
-		int resultado = check;
-		for (int i = check - 1; i > 0; --i) {
-			resultado *= i;
-		}
-		return resultado;
+		return(num * factorial(num - 1));
+	}
+	
+}
+
+//Esta función es usada por el ejercicio seis, y devuelve verdadero si se cumplen las condiciones de noun() y verb()
+bool sentence() {
+	return noun() && verb();
+}
+
+//Esta función es usada por el ejercicio seis, y devuelve verdadero si encuentra uno de los nexos encontrados la gramatica.
+bool conjunction() {
+	string s = ss.get();
+	if (s == "and" || s == "or" || s == "but") {
+		return true;
+	}
+	else {
+		return false;
 	}
 }
 
-string sentence() {
-	return "";
+//Esta función es usada por el ejercicio seis, y devuelve verdadero si encuentra uno de los sujetos encontrados la gramatica.
+bool noun() {
+	string s = ss.get();
+	if (s == "birds" || s == "fish" || s == "C++") {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
-string conjunction() {
-	return "";
-}
-
-string noun() {
-	return "";
-}
-
-string verb() {
-	return "";
+//Esta función es usada por el ejercicio seis, y devuelve verdadero si encuentra uno de los verbos encontrados la gramatica.
+bool verb() {
+	string s = ss.get();
+	if (s == "rules" || s == "fly" || s == "swim") {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
